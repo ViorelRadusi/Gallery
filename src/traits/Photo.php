9aea45a45382ad6870310bbb871c4971d1cd6314
Viorel RADUSI <vio@Dev-iMac-20-Radush.local>
2015-02-23 15:03:31 +0200

    delete directory

--- src/traits/Photo.php
+++ src/traits/Photo.php
@@ -5,4 +5,12 @@ trait Photo {
     return $this->morphMany('\Request\Gallery\Models\Rg_photo', 'imageable');
   }
 
+  
+  public function delete(){
+   $path =  public_path() . Config::get("gallery::uploadPath") . Str::slug($this->photos->first()->imageable_type) . '/' . $this->name . '-' . $this->id ;
+   File::deleteDirectory($path);
+   $this->photos()->delete();
+   parent::delete();
+  }
+
 }
